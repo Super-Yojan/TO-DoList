00e6d542a518a56efc385ff1e432e0dc5f0186d4
Super-Yojan <gautamyojan0@gmail.com>
2019-12-20 06:36:31 -0500

    used session to imporve login

---
+++ todo.py
@@ -0,0 +1,65 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from databaseSetup import item, Base, User
+
+engine = create_engine('sqlite:///todolist.db')
+Base.metadata.bind = engine
+
+Session = sessionmaker(bind=engine)
+
+
+def add_item( user_id , items):
+    session = Session()
+    newitem = item(items=items, user_id=user_id)
+    session.add(newitem)
+    session.commit()
+
+
+def show_items(user_id):
+    session = Session()
+    items = session.query(item).filter_by(user_id=user_id).all()
+    return items
+
+
+def show_item(user_id, item_id):
+    session = Session()
+    one_item = session.query(item).filter_by(user_id=user_id, id = item_id).one()
+    return one_item
+
+
+def login_verification(email , password):
+    session = Session()
+    try:
+        user = session.query(User).filter_by(email=email , password=password).one()
+        return user
+    except :
+        return None
+
+
+def create_account(first_name , last_name, email, password):
+    session = Session()
+    oldUser = session.query(User).filter_by(email=email).all()
+    if len(oldUser) == 0:
+        newUser = User(first_name=first_name, last_name=last_name,
+                       email=email,
+                       password=password)
+        session.add(newUser)
+        session.commit()
+        return True
+    else:
+        return False
+
+
+def delete_item(user_id , id):
+    session = Session()
+    items = session.query(item).filter_by(user_id=user_id, id=id).one()
+    session.delete(items)
+    session.commit()
+
+
+def edit_item(user_id, id, edit_item):
+    session = Session()
+    items = session.query(item).filter_by(id=id, user_id=user_id).one()
+    items.items = edit_item
+    session.add(items)
+    session.commit()
--- todoController.py
+++ todoController.py
@@ -1,107 +1,101 @@
-from flask import Flask, render_template, request, url_for, redirect, g
-from flask import session as sn
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from databaseSetup import item, Base , User
+from flask import Flask, render_template, request, url_for, redirect, session, escape
+import todo
 import os
 
 app = Flask('__name__')
-engine = create_engine('sqlite:///todolist.db')
-Base.metadata.bind = engine
 
-Session = sessionmaker(bind=engine)
+app.secret_key = os.urandom(24)
 
-app.secret_key= os.urandom(24)
 
 @app.route('/')
 def new_todo():
-    return render_template('login.html')
-
-
-@app.route('/addItem/<int:user_id>' , methods=['GET', 'POST'])
-def add_item(user_id):
-    session = Session()
-    if request.method == 'POST':
-        newitem = item(items = request.form['item'], user_id = user_id)
-        session.add(newitem)
-        session.commit()
-        return render_template('main.html', user_id = user_id)
+    if 'user_id' in session:
+        return redirect(url_for('show_item'))
     else:
-        return render_template('main.html', user_id = user_id)
-
-@app.route('/showItem/<int:user_id>', methods=['GET','POST'])
-def show_item(user_id):
-    session = Session()
-    if g.user:
-        items = session.query(item).filter_by(user_id = user_id).all()
-        return render_template('show.html', items=items , user_id = user_id)
-    else:
-        return redirect(url_for('login'))
-
-@app.before_request
-def before_request():
-    g.user = None
-    if sn:
-        g.user = sn['user']
-
-@app.route('/editItem/<int:id>/<int:user_id>' , methods=['GET', 'POST'])
-def edit_item(id , user_id):
-    session = Session()
-    items = session.query(item).filter_by(id = id , user_id = user_id).one()
-    if request.method == 'POST':
-        if request.form['item']:
-            items.items = request.form['item']
-            session.add(items)
-            session.commit()
-        return redirect(url_for('show_item' , user_id = user_id))
-    else:
-        return render_template('edit.html', item = items , user_id = user_id)
-
-
-@app.route('/deleteItem/<int:user_id>/<int:id>')
-def delete_item( user_id, id):
-    session = Session()
-    items = session.query(item).filter_by(user_id=user_id, id=id).one()
-    session.delete(items)
-    session.commit()
-    return redirect(url_for('show_item' , user_id = user_id ))
-
-
-@app.route('/login' , methods=['GET' , 'POST'])
-def login():
-    session = Session()
-    sn.pop('user', None)
-    try:
-        user = session.query(User).filter_by(email = request.form['email'] , password = request.form['password']).one()
-        sn['user'] = request.form['email']
-        return redirect(url_for('show_item', user_id=user.id))
-    except:
         return render_template('login.html')
 
 
-@app.route('/createacc' , methods=['GET', 'POST'])
-def create_acc():
-    session = Session()
+@app.route('/addItem/', methods=['GET', 'POST'])
+def add_item():
     if request.method == 'POST':
-        oldUser = session.query(User).filter_by(email = request.form['email']).all()
-        if len(oldUser) == 0:
-            newUser = User(first_name = request.form['first_name'], last_name=request.form['last_name'], email=request.form['email'],
-                           password = request.form['password'])
-            session.add(newUser)
-            session.commit()
+        if 'user_id' in session:
+            user_id = session['user_id']
+            todo.add_item(user_id=user_id, items=request.form['item'])
+        return render_template('main.html')
+    else:
+        return render_template('main.html')
+
+
+@app.route('/editItem/<int:id>', methods=['GET', 'POST'])
+def edit_item(id):
+    if request.method == 'POST':
+        if request.form['item']:
+            if 'user_id' in session:
+                user_id = session['user_id']
+                todo.edit_item(user_id=user_id, id=id, edit_item=request.form['item'])
+
+        return redirect(url_for('show_item', user_id=session['user_id']))
+    else:
+        if 'user_id' in session:
+            user_id = session['user_id']
+            item = todo.show_item(user_id=user_id , item_id=id)
+            return render_template('edit.html', user_id=user_id, item=item)
+        else:
+            return redirect(url_for('new_todo'))
+
+
+@app.route('/deleteItem/<int:id>')
+def delete_item(id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        todo.delete_item(user_id=user_id, id=id)
+        return redirect(url_for('show_item', user_id=user_id))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/showItem/', methods=['GET', 'POST'])
+def show_item():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        items = todo.show_items(user_id=user_id)
+        return render_template('show.html', items=items, user_id=user_id)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    session.pop('user_id', None)
+
+    user = todo.login_verification(email=request.form['email'], password=request.form['password'])
+
+    if user is not None:
+        session['user_id'] = user.id
+        return redirect(url_for('show_item'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/createacc', methods=['GET', 'POST'])
+def create_acc():
+    if request.method == 'POST':
+        if todo.create_account(first_name=request.form['first_name'], last_name=request.form['last_name'],
+                               email=request.form['email'],
+                               password=request.form['password']) == True:
             return redirect(url_for('login'))
         else:
-            return render_template('createacc.html', error = "Email already exists. Please use a different one")
+            return render_template('createacc.html', error="Email already exists. Please use a different one")
     else:
         return render_template('createacc.html')
 
+
 @app.route('/logout')
 def logout():
-    sn.pop('user', None)
+    session.pop('user_id', None)
     return redirect(url_for('new_todo'))
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
